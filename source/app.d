5de056527d4c54971bb8a1fe9e221f90533350bb
Ikey Doherty <ikey@serpentos.com>
2023-02-02 18:17:04 +0000

    Add the initial application structure
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

---
+++ source/app.d
@@ -0,0 +1,24 @@
+/*
+ * SPDX-FileCopyrightText: Copyright © 2020-2023 Ikey Doherty
+ *
+ * SPDX-License-Identifier: Zlib
+ */
+
+/**
+ * app
+ *
+ * Main binary for the ics-experiment
+ *
+ * Authors: Copyright © 2020-2023 Ikey Doherty
+ * License: Zlib
+ */
+
+module app;
+
+/**
+ * Main entry point into ics-experiment
+ */
+void main() @safe
+{
+
+}
---
+++ source/ics/package.d
@@ -0,0 +1,16 @@
+/*
+ * SPDX-FileCopyrightText: Copyright © 2020-2023 Ikey Doherty
+ *
+ * SPDX-License-Identifier: Zlib
+ */
+
+/**
+ * ics
+ *
+ * ICS (iCalendar File) parsing support
+ *
+ * Authors: Copyright © 2020-2023 Ikey Doherty
+ * License: Zlib
+ */
+
+module ics;
